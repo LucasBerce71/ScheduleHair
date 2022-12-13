9dbc8cce1a89d6a2cba01a4985d10c9015874bae
Sabrina Daudt Teixeira <Sabrina.daudt@hotmail.com>
2022-12-13 00:23:01 -0300

    botão na tela login

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,16 @@
 import React, { ButtonHTMLAttributes } from "react";
+import { Container } from "./styles";
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
 }
 
 function Button({ isLoading, children, ...rest }: IButtonProps) {
-  
+  return( 
+    <Container> 
+      Entrar      
+    </Container> 
+  )
 }
+
+export default Button
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { Container, Content, Background } from "./styles";
 
@@ -10,6 +11,7 @@ function SignIn () {
         <h1>Faça seu login</h1>
         <Input name="Login" />
         <Input name="Senha" />
+        <Button/>
       </Content>
       <Background />
     </Container>
